dd72cc9a44a2f5a745f427a278bcc60ed7a3a36a
Victor Nunez <nunez.ve@gmail.com>
2019-03-29 10:29:45 -0400

    Removes old project

--- GimbalSDK.iOS-Unified/libGimbal.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("libGimbal.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true)]
--- GimbalSDK.iOS-Unified/libGimbalExperience.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("libGimbalExperience.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true)]
